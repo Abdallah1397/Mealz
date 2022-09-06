5cde22054868f5fb82e84608877e3ee68ecad345
Abdallah Mahmoud Ahmed Mahmoud Attallah <Abdallah.Attallah@Egypt.TE-Data.core>
2022-09-06 14:23:36 +0200

    Creation of join us component

--- src/components/JoinUs/joinUs.js
+++ src/components/JoinUs/joinUs.js
@@ -1,13 +1,33 @@
-import './joinUs.css';
-
-
+import "./joinUs.css";
+import Burger from "../../assets/images/JoinUs/burger.svg";
+import drivers from "../../assets/images/JoinUs/drivers.svg";
+import partner from "../../assets/images/JoinUs/partners.svg";
+import jobs from "../../assets/images/JoinUs/jobs.svg";
 
 // Join Us Component
-const JoinUs=()=>{
-    return(
-        <div className='joinUs'>
-
+const JoinUs = () => {
+  return (
+    <div className="joinUs">
+      {/* Burger Image */}
+      <img src={Burger} alt="Burger Image" className="joinUs-burgerImage" />
+      {/* Headind*/}
+      <h1 className="joinUs-headingTitle">Join Us</h1>
+      {/* Div Icons */}
+      <div className="joinUs-Icons">
+        <div className="icon">
+            <img src={partner} alt="Partner Image"/>
+            <h2>Partners</h2>
         </div>
-    )
-}
+        <div className="icon">
+            <img src={drivers} alt="Drivers Image"/>
+            <h2>Drivers</h2>
+        </div>
+        <div className="icon">
+            <img src={jobs} alt="Jobs Image"/>
+            <h2>Jobs</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default JoinUs;
