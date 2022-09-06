fc16c2f1cf0c932d337487bcd9553f3f824f05a0
Abdallah Mahmoud Ahmed Mahmoud Attallah <Abdallah.Attallah@Egypt.TE-Data.core>
2022-09-06 11:38:38 +0200

    Handle download section view

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import './App.css';
-import AppSection from './components/AppSection/appSection';
-import Hero from './components/Hero/hero';
-import Navbar from './components/Nav/nav';
-
+import "./App.css";
+import AppSection from "./components/AppSection/appSection";
+import Hero from "./components/Hero/hero";
+import JoinUs from "./components/JoinUs/joinUs";
+import Navbar from "./components/Nav/nav";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <Hero />
-      <AppSection/>
+      <AppSection />
+      <JoinUs />
     </div>
   );
 }
---
+++ src/components/JoinUs/joinUs.js
@@ -0,0 +1,13 @@
+import './joinUs.css';
+
+
+
+// Join Us Component
+const JoinUs=()=>{
+    return(
+        <div className='joinUs'>
+
+        </div>
+    )
+}
+export default JoinUs;
